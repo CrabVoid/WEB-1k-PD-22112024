4191c1a9d9046d057536c0b6256aca87043fbd58
Crab_Void <you@example.com>
2024-11-25 09:18:14 +0200

    i hope calculator works

--- basics.js
+++ basics.js
@@ -1,3 +1,20 @@
-let isStudent = true;
+/*let isStudent = true;
 
+if (isStudent = true) {
+    console.log("STUDENTS!")
+} else {
+    console.log("NAV STUDENTS!")
+}; */
+
+let numbers = [12, 23, 41]
+
+
+
+
+
+
+
+
+function printErrorStatus(text , code) {
     
+}
